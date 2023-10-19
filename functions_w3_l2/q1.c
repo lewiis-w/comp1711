caa2464d24928e11f871aabc44e0e579563531b3
Lewis Worby <146661423+lewiis-w@users.noreply.github.com>
2023-10-19 09:55:51 +0000

    Week 3 lesson 2

---
+++ functions_w3_l2/q1.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+
+float circle_area(float radius)
+{
+    float area;
+    area = 3.14 * radius * radius
+}
+
+int main ()
+printf ("please enter the radius of the circle: ");
+    scanf ("%f", &radius);
